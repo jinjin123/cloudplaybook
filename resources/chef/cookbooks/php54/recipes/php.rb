7fd0f7e53079f37774bb695bb0ff117e3ba5186c
keithyau <keithyau.hk@gmail.com>
2014-01-11 19:06:36 +0800

    allow php install drush if it does not exist

--- resources/chef/cookbooks/php54/recipes/php.rb
+++ resources/chef/cookbooks/php54/recipes/php.rb
@@ -46,3 +46,9 @@ end
 service "httpd" do
 	action [:enable,:start]
 end
+
+execute "installDrushIfNotExist" do
+        command "wget --quiet -O - http://ftp.drupal.org/files/projects/drush-7.x-5.9.tar.gz | tar -zxf - -C /usr/local/share; ln -s /usr/local/share/drush/drush /usr/local/bin/drush; drush"
+        cwd "#{node[:awsex][:appdir]}/"
+        not_if { ::File.exists?("/usr/local/bin/drush") }
+end
