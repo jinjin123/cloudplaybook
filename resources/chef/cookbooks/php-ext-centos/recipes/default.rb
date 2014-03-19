fe759548c0f7989b780303039b2807c09cf345be
keithyau <keithyau.hk@gmail.com>
2014-03-19 18:57:42 +0800

    add php-ext-centos cookbook

---
+++ resources/chef/cookbooks/php-ext-centos/recipes/default.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook Name:: php-ext-centos
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+include_recipe "php-ext-centos::ext"
---
+++ resources/chef/cookbooks/php-ext-centos/recipes/ext.rb
@@ -0,0 +1,33 @@
+execute "prepareformcrypt" do
+        command "mkdir -p /root/.ssh"
+end
+
+remote_file "#{Chef::Config[:file_cache_path]}/externalsrc.rpm" do
+    source "http://mirror.us.leaseweb.net/epel/6/x86_64/epel-release-6-8.noarch.rpm"
+    action :create
+end
+
+if platform?("centos")
+  rpm_package "externalsrc" do
+    source "#{Chef::Config[:file_cache_path]}/externalsrc.rpm"
+    action :install
+  end
+end
+
+if platform?("amazon")
+  rpm_package "externalsrc" do
+    source "#{Chef::Config[:file_cache_path]}/externalsrc.rpm"
+    action :upgrade
+  end
+end
+
+pkgs = [ 'php-cli', 'php-gd', 'php-mbstring', 'php-mcrypt', 'php-pdo', 'php-xml', 'php-xmlrpc', 'php-mysql','php-pear','php-devel','zlib-devel','libevent','libevent-devel' ]
+pkgs.each do |pkg|
+  package pkg do
+    action :install
+  end
+end
+
+service "httpd" do
+	action [:enable,:start]
+end
