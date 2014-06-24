634b9623a8778c2b03fcf2a32e9ff5628ea7dac8
Horizonlink <horizonlink@Horizonlinks-Mac-mini.local>
2014-06-24 06:41:31 +0000

    Fine tuning db_deploy.sh

--- resources/scripts/Deploy/db_deploy.sh
+++ resources/scripts/Deploy/db_deploy.sh
@@ -10,8 +10,9 @@ db_address=
 db_name=
 db_user=
 db_passwd=
+way=
 
-while getopts r:f:d:n:m:w: opt
+while getopts r:f:d:n:m:w:l: opt
 do
         case $opt in
                 r)      region=$OPTARG;;
@@ -20,12 +21,17 @@ do
                 n)      db_name=$OPTARG;;
                 m)      db_user=$OPTARG;;
                 w)      db_passwd=$OPTARG;;
+                l)      way=$OPTARG
                 *)      echo "-$opt not recognized";;
         esac
 done
 
+if [ $way == 'file' ];then
 # Get the SQL file from S3
 /usr/bin/s3cmd get "s3://gamecloud-"$region"/"$file_id ./sql --force
-
 # Apply the SQL file to mySQL DB
-/usr/bin/mysql -h $db_address -u $db_name "-p"$db_passwd < "./sql/"$file_id
+/usr/bin/mysql -h $db_address -u $db_user "-p"$db_passwd $db_name < "./sql/"$file_id
+elif [ $way == 'url' ];then
+# Stream the remote SQL file to MySQL
+curl $file_id | /usr/bin/mysql -h $db_address -u $db_user "-p"$db_passwd $db_name
+fi;
