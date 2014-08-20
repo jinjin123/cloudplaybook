54759d91279cb784d22f379f05cd7f63037e0442
Horizonlink <horizonlink@Horizonlinks-Mac-mini.local>
2014-08-20 07:11:05 +0000

    Add composer cookbook

---
+++ resources/chef/cookbooks/composer/recipes/install.rb
@@ -0,0 +1,19 @@
+script "install" do
+        interpreter "bash"
+        user "root"
+        code <<-EOH
+        cd /root
+        curl -sS https://getcomposer.org/installer | php
+        composer global require drush/drush:6.*
+        sed -i '1i export PATH="$HOME/.composer/vendor/bin:$PATH"' $HOME/.bashrc
+        source $HOME/.bashrc
+        EOH
+end
+
+script "ch_config" do
+        interpreter "bash"
+        user "root"
+        code <<-EOH
+        echo "cgi.fix_pathinfo=0" >> /etc/php.ini
+        EOH
+end
