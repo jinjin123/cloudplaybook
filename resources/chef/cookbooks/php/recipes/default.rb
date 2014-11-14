f38e84cd2ad399cc47e10171eef13821e97403fb
Jinjin123 <1293813551@qq.com>
2014-11-14 20:17:51 +0800

    updating php cookbook to be php5.5

--- resources/chef/cookbooks/php/recipes/default.rb
+++ resources/chef/cookbooks/php/recipes/default.rb
@@ -1,11 +1,8 @@
 #
-# Author::  Joshua Timberman (<joshua@opscode.com>)
-# Author::  Seth Chisamore (<schisamo@opscode.com>)
-# Cookbook Name:: php
-# Recipe:: default
-#
-# Copyright 2009-2011, Opscode, Inc.
-#
+# Author:: Adar Porat(<adar.porat@gmail.com>)
+# Cookbook Name:: php55
+# Attribute:: default
+##
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -19,21 +16,67 @@
 # limitations under the License.
 #
 
-include_recipe "php::#{node['php']['install_method']}"
+case node[:platform]
+    
+    when "amazon"
+    node.set['php']['packages'] = ['php55w', 'php55w-devel', 'php55w-cli', 'php55w-snmp', 'php55w-soap', 'php55w-xml', 'php55w-xmlrpc', 'php55w-process', 'php55w-mysqlnd', 'php55w-pecl-memcache', 'php55w-opcache', 'php55w-pdo', 'php55w-imap', 'php55w-mbstring','php55-gd']
+    node.set['mysql']['server']['packages'] = %w{mysql55-server}
+    node.set['mysql']['client']['packages'] = %w{mysql55}
+    
+    # add the webtatic repository
+    yum_repository "webtatic" do
+        repo_name "webtatic"
+        description "webtatic Stable repo"
+        url "http://repo.webtatic.com/yum/el6/x86_64/"
+        key "RPM-GPG-KEY-webtatic-andy"
+        action :add
+    end
+    
+    yum_key "RPM-GPG-KEY-webtatic-andy" do
+        url "http://repo.webtatic.com/yum/RPM-GPG-KEY-webtatic-andy"
+        action :add
+    end
+    
+    
+    # remove any existing php/mysql
+    execute "yum remove -y php* mysql*"
+    
+    # get the metadata
+    execute "yum -q makecache"
+    
+    # manually install php 5.5....
+    execute "yum install -y --skip-broken php55w php55w-devel php55w-cli php55w-snmp php55w-soap php55w-xml php55w-xmlrpc php55w-process php55w-mysqlnd php55w-pecl-memcache php55w-opcache php55w-pdo php55w-imap php55w-mbstring"
 
-# update the main channels
-php_pear_channel 'pear.php.net' do
-  action :update
-end
+  when "rhel", "fedora", "suse", "centos"
+  # add the webtatic repository
+  yum_repository "webtatic" do
+    repo_name "webtatic"
+    description "webtatic Stable repo"
+    url "http://repo.webtatic.com/yum/el6/x86_64/"
+    key "RPM-GPG-KEY-webtatic-andy"
+    action :add
+  end
 
-php_pear_channel 'pecl.php.net' do
-  action :update
-end
+  yum_key "RPM-GPG-KEY-webtatic-andy" do
+    url "http://repo.webtatic.com/yum/RPM-GPG-KEY-webtatic-andy"
+    action :add
+  end
+  
+  node.set['php']['packages'] = ['php55w', 'php55w-devel', 'php55w-cli', 'php55w-snmp', 'php55w-soap', 'php55w-xml', 'php55w-xmlrpc', 'php55w-process', 'php55w-mysqlnd', 'php55w-pecl-memcache', 'php55w-opcache', 'php55w-pdo', 'php55w-imap', 'php55w-mbstring']
+  node.set['mysql']['server']['packages'] = %w{mysql55-server}
+  node.set['mysql']['client']['packages'] = %w{mysql55}
+  
+  include_recipe "php"
 
-php_pear "apc" do
-  action :install
-  directives(:enable_cli => 1)
-end
-
-include_recipe "php::ini"
-include_recipe "php::apc"
+  when "debian"
+    include_recipe "apt"
+	apt_repository "wheezy-php55" do
+		uri "#{node['php55']['dotdeb']['uri']}"
+		distribution "#{node['php55']['dotdeb']['distribution']}-php55"
+		components ['all']
+		key "http://www.dotdeb.org/dotdeb.gpg"
+		action :add
+	end
+	
+	  include_recipe "php"
+  end
