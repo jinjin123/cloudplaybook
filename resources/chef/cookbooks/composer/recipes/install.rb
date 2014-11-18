b5c57bf0fc7fdfec0ca73acb5e2f5d78bdf1f1c7
Jinjin123 <1293813551@qq.com>
2014-11-18 19:53:45 +0800

    Changing composer to install drush 7 globally

--- resources/chef/cookbooks/composer/recipes/install.rb
+++ resources/chef/cookbooks/composer/recipes/install.rb
@@ -5,9 +5,16 @@ script "install" do
         cd /root
         curl -sS https://getcomposer.org/installer | php
 	mv composer.phar /usr/local/bin/composer
-        /usr/local/bin/composer global require drush/drush:6.*
-        sed -i '1i export PATH="$HOME/.composer/vendor/bin:$PATH"' $HOME/.bashrc
-        source $HOME/.bashrc
+        ln -s /usr/local/bin/composer /usr/bin/composer
+        mkdir -p /usr/local/src/drush
+        git clone --depth 1 https://github.com/drush-ops/drush.git /usr/local/src/drush
+        cd /usr/local/src/drush
+        git checkout 7.0.0-alpha5  #or whatever version you want.
+        ln -s /usr/local/src/drush/drush /usr/bin/drush
+        composer install
+#        /usr/local/bin/composer global require drush/drush::dev-master
+#        sed -i '1i export PATH="$HOME/.composer/vendor/bin:$PATH"' $HOME/.bashrc
+#        source $HOME/.bashrc
         EOH
 end
 
