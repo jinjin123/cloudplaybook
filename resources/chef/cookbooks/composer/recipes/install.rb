b1e34f3ed5f5400caa378ff1f41ce6cb6d16fb62
Jinjin123 <1293813551@qq.com>
2014-12-16 18:26:53 +0800

    update of composer cookbook to install in all three user

--- resources/chef/cookbooks/composer/recipes/install.rb
+++ resources/chef/cookbooks/composer/recipes/install.rb
@@ -4,32 +4,34 @@ script "download_composer" do
         code <<-EOH
         cd /root
         curl -sS https://getcomposer.org/installer | php
-	mv composer.phar /usr/local/bin/composer
+	    mv composer.phar /usr/local/bin/composer
         ln -s /usr/local/bin/composer /usr/bin/composer
         cp /home/ec2-user/.b* /var/lib/nginx
         chown nginx:nginx /var/lib/nginx/.b*
         EOH
 end
 
-script "install_drush_nginx" do
-        interpreter "bash"
+execute "install_drush_nginx" do
         user "nginx"
-        code <<-EOH
-        cd
+        group "nginx"
+        environment ({'HOME' => '/var/lib/nginx', 'USER' => 'nginx'})
+        command <<-EOH
+        source /var/lib/nginx/.bashrc
         /usr/local/bin/composer global require drush/drush:dev-master
-        sed -i '1i export PATH="$HOME/.composer/vendor/bin:$PATH"' $HOME/.bashrc
-        source $HOME/.bashrc
+        sed -i '1i export PATH="$HOME/.composer/vendor/bin:$PATH"' ~/.bashrc
+        source ~/.bashrc
         EOH
 end
 
-script "install_drush_ec2user" do
-        interpreter "bash"
+execute "install_drush_ec2user" do
         user "ec2-user"
-        code <<-EOH
-        cd
+        group "ec2-user"
+        environment ({'HOME' => '/home/ec2-user', 'USER' => 'ec2-user'})
+        command <<-EOH
+        source /home/ec2-user/.bashrc
         /usr/local/bin/composer global require drush/drush:dev-master
-        sed -i '1i export PATH="$HOME/.composer/vendor/bin:$PATH"' $HOME/.bashrc
-        source $HOME/.bashrc
+        sed -i '1i export PATH="$HOME/.composer/vendor/bin:$PATH"' ~/.bashrc
+        source ~/.bashrc
         EOH
 end
 
