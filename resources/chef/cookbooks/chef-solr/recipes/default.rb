401bd099c561f30f3bf01e94c95d465df7beecb6
Jinjin123 <1293813551@qq.com>
2014-12-22 19:05:45 +0800

    Adding solr config for drupal into chef-solr cookbook

--- resources/chef/cookbooks/chef-solr/recipes/default.rb
+++ resources/chef/cookbooks/chef-solr/recipes/default.rb
@@ -34,6 +34,94 @@ directory node['solr']['data_dir'] do
   action :create
 end
 
+template 'synonyms.txt' do
+  source 'synonyms.txt'
+  path "#{extract_path}/example/solr/collection1/conf/synonyms.txt"
+  owner 'root'
+  group 'root'
+  mode '0755'
+end
+
+template 'schema.xml' do
+  source 'schema.xml'
+  path "#{extract_path}/example/solr/collection1/conf/schema.xml"
+  owner 'root'
+  group 'root'
+  mode '0755'
+end
+
+template 'stopwords.txt' do
+  source 'stopwords.txt'
+  path "#{extract_path}/example/solr/collection1/conf/stopwords.txt"
+  owner 'root'
+  group 'root'
+  mode '0755'
+end
+
+template 'solrcore.properties' do
+  source 'solrcore.properties'
+  path "#{extract_path}/example/solr/collection1/conf/solrcore.properties"
+  owner 'root'
+  group 'root'
+  mode '0755'
+end
+
+template 'solrconfig_extra.xml' do
+  source 'solrconfig_extra.xml'
+  path "#{extract_path}/example/solr/collection1/conf/solrconfig_extra.xml"
+  owner 'root'
+  group 'root'
+  mode '0755'
+end
+
+template 'solrconfig.xml' do
+  source 'solrconfig.xml'
+  path "#{extract_path}/example/solr/collection1/conf/solrconfig.xml"
+  owner 'root'
+  group 'root'
+  mode '0755'
+end
+
+template 'schema_extra_types.xml' do
+  source 'schema_extra_types.xml'
+  path "#{extract_path}/example/solr/collection1/conf/schema_extra_types.xml"
+  owner 'root'
+  group 'root'
+  mode '0755'
+end
+
+template 'schema_extra_fields.xml' do
+  source 'schema_extra_fields.xml'
+  path "#{extract_path}/example/solr/collection1/conf/schema_extra_fields.xml"
+  owner 'root'
+  group 'root'
+  mode '0755'
+end
+
+template 'protwords.txt' do
+  source 'protwords.txt'
+  path "#{extract_path}/example/solr/collection1/conf/protwords.txt"
+  owner 'root'
+  group 'root'
+  mode '0755'
+end
+
+template 'mapping-ISOLatin1Accent.txt' do
+  source 'mapping-ISOLatin1Accent.txt'
+  path "#{extract_path}/example/solr/collection1/conf/mapping-ISOLatin1Accent.txt"
+  owner 'root'
+  group 'root'
+  mode '0755'
+end
+
+template 'elevate.xml' do
+  source 'elevate.xml'
+  path "#{extract_path}/example/solr/collection1/conf/elevate.xml"
+  owner 'root'
+  group 'root'
+  mode '0755'
+end
+
 template '/var/lib/solr.start' do
   source 'solr.start.erb'
   owner 'root'
