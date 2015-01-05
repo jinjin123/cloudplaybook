265aad9de16fb73f68285cc8de1f47f843f901f1
Jinjin123 <1293813551@qq.com>
2015-01-05 15:36:07 +0800

    in diskmount cookbook, wait until drupal sites/default/file is created

--- resources/chef/cookbooks/diskmount/recipes/default.rb
+++ resources/chef/cookbooks/diskmount/recipes/default.rb
@@ -12,6 +12,10 @@
 #       command "mkdir -p #{node[:diskmount][:localsourcefolder]}"
 #end
 
+execute "wait_until_drupal" do
+        command "n=0;until [ $n -ge 5 ];do ls -lrt #{node[:diskmount][:localsourcefolder]}; [ $? -eq 0 ] && break;n=$[$n+1];sleep 15;done;"
+end
+
 execute "mountvolume" do
 	command "echo '#{node[:diskmount][:glusterserverip]}:/#{node[:diskmount][:glustervolume]} #{node[:diskmount][:localsourcefolder]} glusterfs defaults 0 0' >>/etc/fstab;"
 	not_if "cat /proc/mounts | grep glusterfs"
