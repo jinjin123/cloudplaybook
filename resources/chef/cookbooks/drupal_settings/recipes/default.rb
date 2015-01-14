db2d0d559a0be68b20b8802e27d1f26609d4760c
Jinjin123 <1293813551@qq.com>
2015-01-14 17:31:37 +0800

    Calling all planned settings.php

--- resources/chef/cookbooks/drupal_settings/recipes/default.rb
+++ resources/chef/cookbooks/drupal_settings/recipes/default.rb
@@ -39,31 +39,160 @@ fi
 EOH
 end
 
-#template "/var/www/html/sites/default/settings.php" do
-#        source "settings.php"
-#        mode 0600
-#        retries 3
-#        retry_delay 30
-#        owner "nginx"
-#        group "nginx"
-#        action :create
-#        ignore_failure true
-#end rescue NoMethodError
-
 drupal_secret = Chef::EncryptedDataBagItem.load_secret("#{node['drupal_settings']['secretpath']}")
 
+template "/var/www/html/sites/default/basic.settings.php" do
+  source "basic.settings.php"
+  mode 0600
+  retries 3
+  retry_delay 30
+  owner "nginx"
+  group "nginx"
+  action :create
+  ignore_failure true
+end rescue NoMethodError
+
+# Check if databag exists before applying templates
 if Chef::DataBag.list.key?('drupal')
+  # Check if DataBag item exist before applying templates
+  if Chef::DataBagItem.validate_id!('Database')
+    Database_Setting = Chef::EncryptedDataBagItem.load("drupal", "Database", drupal_secret)
+    template "/var/www/html/sites/default/settings.php" do
+      source "settings.php"
+      variables(
+        :db_name => Database_Setting['db_name'], 
+        :db_user => Database_Setting['db_user'],
+        :db_passwd => Database_Setting['db_passwd'],
+        :mysql_url => Database_Setting['mysql_url'],
+      )
+      mode 0600
+      retries 3
+      retry_delay 30
+      owner "nginx"
+      group "nginx"
+      action :create
+      ignore_failure true
+    end rescue NoMethodError
+  end
+
+  if Chef::DataBagItem.validate_id!('AWS')
+    AWS_Setting = Chef::EncryptedDataBagItem.load("drupal", "AWS", drupal_secret)
+    template "/var/www/html/sites/default/aws.settings.php" do
+      source "aws.settings.php"
+      variables(
+        :aws_key => AWS_Setting['aws_key'], 
+        :aws_secret => AWS_Setting['aws_secret'],
+      )
+      mode 0600
+      retries 3
+      retry_delay 30
+      owner "nginx"
+      group "nginx"
+      action :create
+      ignore_failure true
+    end rescue NoMethodError
+  end
+  
   if Chef::DataBagItem.validate_id!('Memcache')
-    Memcache_Setting = Chef::EncryptedDataBagItem.load("Memcache", "drupal", drupal_secret)
+    Memcache_Setting = Chef::EncryptedDataBagItem.load("drupal", "Memcache", drupal_secret)
     template "/var/www/html/sites/default/memcache.settings.php" do
       source "memcache.settings.php"
       variables(
-        :Memcache_server1 => Memcache_Setting['Memcache_server1'] 
-        :Memcache_port1 => Memcache_Setting['Memcache_port1']
-        :Memcache_server2 => Memcache_Setting['Memcache_server2']
+        :Memcache_server1 => Memcache_Setting['Memcache_server1'],
+        :Memcache_port1 => Memcache_Setting['Memcache_port1'],
+        :Memcache_server2 => Memcache_Setting['Memcache_server2'],
         :Memcache_port2 => Memcache_Setting['Memcache_port2']
       )
-    end 
+      mode 0600
+      retries 3
+      retry_delay 30
+      owner "nginx"
+      group "nginx"
+      action :create
+      ignore_failure true
+    end rescue NoMethodError
+  end
+
+  if Chef::DataBagItem.validate_id!('CDN')
+    CDN_Setting = Chef::EncryptedDataBagItem.load("drupal", "CDN", drupal_secret)
+    template "/var/www/html/sites/default/cdn.settings.php" do
+      source "cdn.settings.php"
+      variables(
+        :LoadBalancerDNS => CDN_Setting['LoadBalancerDNS']
+      )
+      mode 0600
+      retries 3
+      retry_delay 30
+      owner "nginx"
+      group "nginx"
+      action :create
+      ignore_failure true
+    end rescue NoMethodError
+  end
+  
+  if Chef::DataBagItem.validate_id!('S3CDN')
+    S3CDN_Setting = Chef::EncryptedDataBagItem.load("drupal", "S3CDN", drupal_secret)
+    template "/var/www/html/sites/default/s3cdn.setttings.php" do
+      source "s3cdn.setttings.php.php"
+      variables(
+        :S3CDN => S3CDN_Setting['S3CDN']
+      )
+      mode 0600
+      retries 3
+      retry_delay 30
+      owner "nginx"
+      group "nginx"
+      action :create
+      ignore_failure true
+    end rescue NoMethodError
+  end
+
+  # Calling three templates by one data_bag
+  if Chef::DataBagItem.validate_id!('Host_n_storage')
+    Host_n_storage_Setting = Chef::EncryptedDataBagItem.load("drupal", "Host_n_storage", drupal_secret)
+    template "/var/www/html/sites/default/cookie.settings.php" do
+      source "cookie.settings.php"
+      variables(
+        :LoadBalancerDNS => Host_n_storage_Setting['LoadBalancerDNS']
+      )
+      mode 0600
+      retries 3
+      retry_delay 30
+      owner "nginx"
+      group "nginx"
+      action :create
+      ignore_failure true
+    end rescue NoMethodError
+    
+    template "/var/www/html/sites/default/s3.settings.php" do
+      source "s3.settings.php"
+      variables(
+        :S3bucket => Host_n_storage_Setting['S3bucket']
+      )
+      mode 0600
+      retries 3
+      retry_delay 30
+      owner "nginx"
+      group "nginx"
+      action :create
+      ignore_failure true
+    end rescue NoMethodError
+    
+    template "/var/www/html/sites/default/xmlsitemap.settings.php " do
+      source "xmlsitemap.settings.php "
+      variables(
+        :LoadBalancerDNS => Host_n_storage_Setting['LoadBalancerDNS'],
+        :S3bucket => Host_n_storage_Setting['S3bucket']
+      )
+      mode 0600
+      retries 3
+      retry_delay 30
+      owner "nginx"
+      group "nginx"
+      action :create
+      ignore_failure true
+    end rescue NoMethodError
+    
   end
 end
 
