09bbe2518cf8dd8d5eb3ceec8f3930cede98c395
Jinjin123 <1293813551@qq.com>
2015-01-22 17:06:51 +0800

    Use empty run-list to get chef-vault encrpted secret_key

--- resources/chef/cookbooks/updatevault/recipes/default.rb
+++ resources/chef/cookbooks/updatevault/recipes/default.rb
@@ -51,14 +51,20 @@ execute 'call_chefserver' do
   retry_delay 30
 end
 
+
+chef_gem "chef-vault"
+require "chef-vault"
 if !File.exist?("/etc/chef/secret_key")
-  chef_gem "chef-vault"
-  require "chef-vault"
-  vault = ChefVault::Item.load("secrets", "secret_key")
-  vault['secret_key'] = vault['secret_key'].tr(" ", "\n")
-  # To write changes to the file, use:
-  out_file = File.open("/etc/chef/secret_key", "w")
-  concat_string = vault['secret_key']
- # adding a space into the end of the file to avoid being strip away all text
-  out_file.puts concat_string
+  ruby_block "Chef_Vault" do
+    block do
+      vault = ChefVault::Item.load("secrets", "secret_key")
+      vault['secret_key'] = vault['secret_key'].tr(" ", "\n")
+    # To write changes to the file, use:
+      out_file = File.open("/etc/chef/secret_key", "w")
+      concat_string = vault['secret_key']
+    # adding a space into the end of the file to avoid being strip away all text
+      out_file.puts concat_string
+    end
+    action :run
+  end
 end
