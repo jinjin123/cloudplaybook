f582e38ee02d605f7f0534f17a9a008f62b80bf3
Jinjin123 <1293813551@qq.com>
2015-02-02 22:52:31 +0800

    Sparating drupal and drupal_encrpyt as two data bags calling by chef Server and chef Client

--- resources/scripts/Deploy/data_bag.sh
+++ resources/scripts/Deploy/data_bag.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
 mkdir -p /home/ec2-user/chef11/chef-repo/data_bags/drupal_encrpyt
-tar -xvf /home/ec2-user/drupal_data.tar -C /home/ec2-user/chef11/chef-repo/data_bag/
+if [ ! -f /etc/chef/solo.rb ];
+then
+  cp /home/ec2-user/chef11/chef-repo/.chef/knife.rb /etc/chef/solo.rb
+fi
+tar -xvf /home/ec2-user/drupal_data.tar -C /home/ec2-user/chef11/chef-repo/data_bags/
 #drupal_encrpty
 cd /home/ec2-user/chef11/chef-repo
 /opt/chef-server/embedded/bin/knife data bag create drupal
+/opt/chef-server/embedded/bin/knife data bag create drupal_encrpyt
 for x in `ls data_bags/*.json`
 do
 /opt/chef-server/embedded/bin/knife data bag from file drupal $x --secret-file .chef/secret_key
 done
 #rm .chef/secret_key
 
-mv /home/ec2-user/chef11/chef-repo/data_bag/*.json /home/ec2-user/chef11/chef-repo/data_bags/drupal_encrpyt
+mv /home/ec2-user/chef11/chef-repo/data_bags/*.json /home/ec2-user/chef11/chef-repo/data_bags/drupal_encrpyt
 /usr/bin/chef-solo -o 'recipe[nginx]'
 /usr/bin/chef-solo -j <(echo '{"drupal_settings":{"web_root":"/root/drucloudaws"}, "run_list": "recipe[drupal_settings]"}')
