3c8ae20693cc022eaaf206c74424e77b8827f406
Jinjin123 <1293813551@qq.com>
2015-02-05 18:01:52 +0800

    Changing role's runlist to use php55. fix parameter name in php55-fpm

--- resources/chef/cookbooks/php55-fpm/recipes/default.rb
+++ resources/chef/cookbooks/php55-fpm/recipes/default.rb
@@ -16,9 +16,9 @@ pkgs = %w{
 pkgs.flatten.each do |pkg|
 
   r = package pkg do
-    action( node['php55']['compiletime'] ? :nothing : :install )
+    action( node['php55-fpm']['compiletime'] ? :nothing : :install )
   end
-  r.run_action(:install) if node['php55']['compiletime']
+  r.run_action(:install) if node['php55-fpm']['compiletime']
 
 end
 
