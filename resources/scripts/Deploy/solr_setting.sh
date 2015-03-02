0126df4385a526bb4dbaaea5cf7d52c73590d1db
Jinjin123 <1293813551@qq.com>
2015-03-02 05:23:47 +0000

    Modifying solr_settings.sh for enabling the article search into solr

--- resources/scripts/Deploy/solr_setting.sh
+++ resources/scripts/Deploy/solr_setting.sh
@@ -6,4 +6,6 @@ if ! [[ -z $SOLR_URL && ${SOLR_URL+x} ]]
 then
 #/root/.composer/vendor/bin/drush en apachesolr apachesolr_search -y -r /root/drucloudaws/
 /root/.composer/vendor/bin/drush solr-set-env-url $SOLR_URL -r /root/drucloudaws/
+/root/.composer/vendor/bin/drush sql-query 'INSERT INTO apachesolr_index_bundles (env_id, entity_type, bundle) values ("solr","node","article");' -r /root/drucloudaws/
+/root/.composer/vendor/bin/drush cc all -v -r /root/drucloudaws/
 fi
