0afa0cb9dfa9e2740b310773bb6dbd28b1e3650c
Jinjin123 <1293813551@qq.com>
2015-03-04 07:51:18 +0000

    As solr setting will be overidded by installation of solr, adding drush feature revert all before inserting solr url

--- resources/scripts/Deploy/solr_setting.sh
+++ resources/scripts/Deploy/solr_setting.sh
@@ -5,6 +5,7 @@ sudo echo $SOLR_URL > /var/log/solr_setting.log
 if ! [[ -z $SOLR_URL && ${SOLR_URL+x} ]]
 then
 #/root/.composer/vendor/bin/drush en apachesolr apachesolr_search -y -r /root/drucloudaws/
+/root/.composer/vendor/bin/drush features-revert-all -y /root/drucloudaws/
 /root/.composer/vendor/bin/drush solr-set-env-url $SOLR_URL -r /root/drucloudaws/
 #/root/.composer/vendor/bin/drush sql-query 'INSERT INTO apachesolr_index_bundles (env_id, entity_type, bundle) values ("solr","node","article");' -r /root/drucloudaws/
 /root/.composer/vendor/bin/drush cc all -v -r /root/drucloudaws/
