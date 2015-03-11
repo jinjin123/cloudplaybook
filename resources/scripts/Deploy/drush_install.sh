c5f36a7d6be14731255ca9df8ec26d3e1e11606b
Jinjin123 <1293813551@qq.com>
2015-03-11 14:34:52 +0000

    Wait until the settings.php is exists before running drupal_settings cookbook

--- resources/scripts/Deploy/drush_install.sh
+++ resources/scripts/Deploy/drush_install.sh
@@ -35,6 +35,7 @@ git clone --depth 10 $giturl /root/drucloudaws
 
 cd /root/drucloudaws/sites/default
 /root/.composer/vendor/bin/drush site-install drucloud "--db-url=mysql://"$db_username":"$db_password"@"$db_address"/"$db_name --account-name=admin --account-pass=admin --site-name="drucloudaws" --yes --debug -r /root/drucloudaws/sites/default|| true 
+n=0;until [ $n -ge 5 ];do ls /root/drucloudaws/sites/default/settings.php; [ $? -eq 0 ] && break;n=$[$n+1];sleep 15;done;
 /usr/bin/chef-solo -j <(echo '{"drupal_settings":{"web_root":"/root/drucloudaws","web_user":"root","web_group":"root"}, "run_list": "recipe[drupal_settings]"}') || true
 
 cd /root/drucloudaws/sites/default
