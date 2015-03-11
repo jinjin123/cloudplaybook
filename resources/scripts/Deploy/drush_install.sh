d059958c0d169c958abfa2f631f103a697dd00a9
Jinjin123 <1293813551@qq.com>
2015-03-11 13:59:23 +0000

    Fixing drush_install.sh to make drush installation success

--- resources/scripts/Deploy/drush_install.sh
+++ resources/scripts/Deploy/drush_install.sh
@@ -31,11 +31,12 @@ cd /root
 chmod 400 /root/.ssh/bitbucket
 ssh -i /root/.ssh/bitbucket -o StrictHostKeyChecking=no git@bitbucket.org||true
 mkdir -p /root/drucloudaws
-git clone --depth 10 $giturl /root/drucloudaws >> $LOG
-cd /root/drucloudaws/
+git clone --depth 10 $giturl /root/drucloudaws
 
-/usr/bin/chef-solo -j <(echo '{"drupal_settings":{"web_root":"/root/drucloudaws","web_user":"root","web_group":"root"}, "run_list": "recipe[drupal_settings]"}') || true
 cd /root/drucloudaws/sites/default
-/root/.composer/vendor/bin/drush site-install drucloud --account-name=admin --account-pass=admin --site-name="drucloudaws" --yes || true 
-/root/.composer/vendor/bin/drush cc all
-/root/.composer/vendor/bin/drush php-eval 'node_access_rebuild();'
+/root/.composer/vendor/bin/drush site-install drucloud "--db-url=mysql://"$db_username":"$db_password"@"$db_address"/"$db_name --account-name=admin --account-pass=admin --site-name="drucloudaws" --yes --debug -r /root/drucloudaws/sites/default|| true 
+/usr/bin/chef-solo -j <(echo '{"drupal_settings":{"web_root":"/root/drucloudaws","web_user":"root","web_group":"root"}, "run_list": "recipe[drupal_settings]"}') || true
+
+cd /root/drucloudaws/sites/default
+/root/.composer/vendor/bin/drush cc all -r /root/drucloudaws/sites/default
+/root/.composer/vendor/bin/drush php-eval 'node_access_rebuild();' -r /root/drucloudaws/sites/default
