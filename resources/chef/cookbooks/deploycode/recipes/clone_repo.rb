222af2e46d5b954f9b7189c33f4be4c2a257b9aa
Jinjin123 <1293813551@qq.com>
2015-03-11 09:39:02 +0000

    Update of deploycode cookobook to disable checkout

--- resources/chef/cookbooks/deploycode/recipes/clone_repo.rb
+++ resources/chef/cookbooks/deploycode/recipes/clone_repo.rb
@@ -1,12 +1,12 @@
 # Separating clone_repo into anothe recipe to perform code reuse/function aliked action
 git "clone_repo" do
-     user node[:deploycode][:code_owner]
-           group node[:deploycode][:code_group]
-           repository node[:deploycode][:gitrepo]
-           depth 10
-           retries 3
-           retry_delay 30
-           action :nothing
-           destination node[:deploycode][:localsourcefolder]
-           checkout_branch nil
-     end
+  user node[:deploycode][:code_owner]
+  group node[:deploycode][:code_group]
+  repository node[:deploycode][:gitrepo]
+  depth 10
+  retries 3
+  retry_delay 30
+  action :nothing
+  destination node[:deploycode][:localsourcefolder]
+  enable_checkout false
+end
--- resources/chef/cookbooks/deploycode/recipes/deploy.rb
+++ resources/chef/cookbooks/deploycode/recipes/deploy.rb
@@ -108,7 +108,7 @@ else
 #      reference "master"
       action :sync
       destination node[:deploycode][:localsourcefolder]
-      checkout_branch nil
+      enable_checkout false
       notifies :run, "execute[git_tag]", :immediately
     end        
   else 
