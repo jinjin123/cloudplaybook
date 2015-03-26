a8ddcda0300baa225e914a76aa72e5d683539539
Jinjin123 <1293813551@qq.com>
2015-03-26 11:30:21 +0000

    Adding clear_node into crontab

---
+++ resources/scripts/Deploy/clear_node.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# This script is running by crontab to clear the dummy node exists in chef-server after auto-scaling
+
+source /root/.bashrc
+cd /home/ec2-user/chef11/chef-repo
+NODE_LIST=`/usr/bin/knife node list`
+
+for x in $NODE_LIST
+do
+echo $x
+/opt/chef-server/embedded/bin/knife ssh "name:$x" "echo \"exists!\"" > /tmp/check_chef.txt
+if [ ! `cat /tmp/check_chef.txt|grep exists|wc -l` -eq 1 ];
+then
+/opt/chef-server/embedded/bin/knife node delete $x -y
+fi
+done
+
+if [ -f /tmp/check_chef.txt ];
+then
+  rm /tmp/check_chef.txt
+fi
