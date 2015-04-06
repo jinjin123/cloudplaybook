489a447a7a18f0275d8dd75181a9ccee86f25100
Jinjin123 <1293813551@qq.com>
2015-04-06 17:37:22 +0800

    Update of deploay code script to make deploy to success

--- resources/scripts/Deploy/deploycode.sh
+++ resources/scripts/Deploy/deploycode.sh
@@ -124,7 +124,7 @@ else
     export LANG=en_US.UTF-8
     /opt/chef-server/embedded/bin/knife cookbook upload -a
     sleep 10 
-    /opt/chef-server/embedded/bin/knife ssh "role:chefclient-base" "sudo chef-client -o 'recipe[deploycode]'" || true
+    /opt/chef-server/embedded/bin/knife ssh "name:*" "sudo chef-client -o 'recipe[deploycode]'" || true
   fi
   if [ "$package" = "basic" ]
   then
