d7e91a1ca8d08f95b05ccf8c0f5a737874a32a4a
keithyau <keithyau.hk@gmail.com>
2015-07-24 18:38:58 +0700

    fix epel-testing error

--- resources/chef/cookbooks/yum-epel/attributes/epel-testing.rb
+++ resources/chef/cookbooks/yum-epel/attributes/epel-testing.rb
@@ -13,7 +13,7 @@ else
     default['yum']['epel-testing']['gpgkey'] = 'http://dl.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL'
   when 6
     default['yum']['epel-testing']['description'] = 'Extra Packages for Enterprise Linux 6 - Testing - $basearch'
-    default['yum']['epel-testing']['mirrorlist'] = 'https://mirrors.fedoraproject.org/metalink?repo=testing-epel6&arch=$basearch'
+    default['yum']['epel-testing']['mirrorlist'] = 'http://mirrors.fedoraproject.org/metalink?repo=testing-epel6&arch=$basearch'
     default['yum']['epel-testing']['gpgkey'] = 'http://dl.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL-6r'
   end
 end
