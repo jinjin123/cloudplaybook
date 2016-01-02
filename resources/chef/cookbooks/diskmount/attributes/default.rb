d91c5c14e020a537d861949133d8a6d6ffb5715a
Jinjin123 <1293813551@qq.com>
2016-01-02 15:29:57 +0000

    Committing changes for deploycode and diskmount

--- resources/chef/cookbooks/diskmount/attributes/default.rb
+++ resources/chef/cookbooks/diskmount/attributes/default.rb
@@ -1,5 +1,5 @@
 # gluster fs mount config
 
 default[:diskmount][:localsourcefolder] = "/var/www/shares"
-default[:diskmount][:glusterserverip] = "iptochange"
-default[:diskmount][:glustervolume] = "volumename"
+default[:diskmount][:glusterserverip] = ""
+default[:diskmount][:glustervolume] = ""
