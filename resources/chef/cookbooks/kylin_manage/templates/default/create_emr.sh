386b6f665d6bbe1750d2899381d3e97cf4ee85e8
Jinjin123 <1293813551@qq.com>
2016-08-30 18:55:28 +0000

    Changing recipe name

--- resources/chef/cookbooks/kylin_manage/templates/default/create_emr.sh
+++ resources/chef/cookbooks/kylin_manage/templates/default/create_emr.sh
@@ -8,7 +8,7 @@ VpcId=<%= node[:vpc_id] %>
 EMRSubet=<%= node[:subnet_id] %>
 KEYPAIR=`/home/ec2-user/tools/ec2-metadata -u|grep keyname|cut -d ':' -f2`
 
-/usr/bin/aws emr create-cluster \
+CLUSTER_ID=`/usr/bin/aws emr create-cluster \
 --applications Name=Hadoop Name=Hive Name=Pig Name=Hue Name=HBase Name=ZooKeeper Name=Phoenix Name=HCatalog \
 --tags 'name=kyligence-emr' \
 --ec2-attributes KeyName=$KEYPAIR,InstanceProfile=EMR_EC2_DefaultRole,SubnetId=$EMRSubet \
@@ -18,4 +18,22 @@ KEYPAIR=`/home/ec2-user/tools/ec2-metadata -u|grep keyname|cut -d ':' -f2`
 --log-uri "s3n://aws-logs-810803377174-$REGION/elasticmapreduce/" \
 --name 'Kyligence_Enterprise_demo_architecture' \
 --instance-groups '[{"InstanceCount":2,"InstanceGroupType":"CORE","InstanceType":"m3.xlarge","Name":"Core instance group - 2"},{"InstanceCount":1,"InstanceGroupType":"MASTER","InstanceType":"m3.xlarge","Name":"Master instance group - 1"}]' \
---region $REGION \
+--region $REGION|cut -d':' -f2| sed 's/\"\|,\| //g'`
+
+# Check status and return until success or failed
+STATUS=`/usr/bin/aws emr list-instances --cluster-id $CLUSTER_ID --instance-group-types MASTER|grep \"State\"|cut -d':' -f2| sed 's/\"\|,\| //g'`
+echo -ne "MapReduce creation in progress."
+while [ "$STATUS" != "RUNNING" ]
+do
+  sleep 5
+  echo -ne "."
+  STATUS=`/usr/bin/aws emr list-instances --cluster-id $CLUSTER_ID --instance-group-types MASTER|grep \"State\"|cut -d':' -f2| sed 's/\"\|,\| //g'`
+  if [ "$STATUS" == "AWAITING_FULFILLMENT" ];then
+    echo "AWAITING_FULFILLMENT is responsed, please contact administrator"
+    exit
+  fi
+done
+
+# Print success message
+echo "Creation of EMR completed, you can run update hadoop file"
+
