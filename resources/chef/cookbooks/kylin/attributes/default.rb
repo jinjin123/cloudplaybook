a71e36ab6482eea62ec4e76abc3a21fbb8473b0a
keith Yau <keithyau@bootdev.com>
2016-10-26 10:25:04 +0000

    default.rb edited online with Bitbucket

--- resources/chef/cookbooks/kylin/attributes/default.rb
+++ resources/chef/cookbooks/kylin/attributes/default.rb
@@ -1,7 +1,8 @@
 #source configuration
 default[:kylin][:code_owner] = "hdfs"
 default[:kylin][:code_group] = "hdfs"
-default[:kylin][:kylin_tarball]='https://archive.apache.org/dist/kylin/apache-kylin-1.5.4/apache-kylin-1.5.4-HBase1.x-bin.tar.gz'
+default[:kylin][:kylin_tarball]='https://archive.apache.org/dist/kylin/apache-kylin-1.5.4.1/apache-kylin-1.5.4.1-hbase1.x-bin.tar.gz'
+#default[:kylin][:kylin_tarball]='https://archive.apache.org/dist/kylin/apache-kylin-1.5.4/apache-kylin-1.5.4-HBase1.x-bin.tar.gz'
 #default[:kylin][:kylin_tarball]='https://archive.apache.org/dist/kylin/apache-kylin-1.5.2/apache-kylin-1.5.2-HBase1.x-bin.tar.gz'
 #default[:kylin][:kylin_tarball]='https://archive.apache.org/dist/kylin/apache-kylin-1.5.2.1/apache-kylin-1.5.2.1-HBase1.x-bin.tar.gz'
 
