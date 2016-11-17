f30c46ecad36d1fac385a2326737d30e5b56fe9a
keith Yau <keithyau@bootdev.com>
2016-11-17 16:33:05 +0000

    default.rb edited online with Bitbucket
    add KAP link

--- resources/chef/cookbooks/kylin/attributes/default.rb
+++ resources/chef/cookbooks/kylin/attributes/default.rb
@@ -2,6 +2,8 @@
 default[:kylin][:code_owner] = "hdfs"
 default[:kylin][:code_group] = "hdfs"
 default[:kylin][:kylin_tarball]='https://s3.cn-north-1.amazonaws.com.cn/bootdevcn/apache-kylin-1.5.4.1-hbase1.x-bin.tar.gz'
+#Path of KAP
+#https://olapio.sharepoint.com/products/_layouts/15/guestaccess.aspx?guestaccesstoken=jipVmtq%2fT6%2b2Uu3ZGhVlV2CCFWXtiJsueBxJU0otHFI%3d&docid=044c0dbd8160c4a11a73264001f658b4c&rev=1
 #default[:kylin][:kylin_tarball]='https://archive.apache.org/dist/kylin/apache-kylin-1.5.4/apache-kylin-1.5.4-HBase1.x-bin.tar.gz'
 #default[:kylin][:kylin_tarball]='https://archive.apache.org/dist/kylin/apache-kylin-1.5.2/apache-kylin-1.5.2-HBase1.x-bin.tar.gz'
 #default[:kylin][:kylin_tarball]='https://archive.apache.org/dist/kylin/apache-kylin-1.5.2.1/apache-kylin-1.5.2.1-HBase1.x-bin.tar.gz'
