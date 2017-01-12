7f5658a3531f0dd02398c03f5274114f6720886e
root <keithyau.hk@gmail.com>
2017-01-12 09:23:35 +0000

    add templates

---
+++ resources/chef/cookbooks/drupalsetting/templates/default/reportingsys.externa_connection_db.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * SQL数据库配置信息
+ * 多库
+ * version
+ */
+ $GLOBALS['arr'] = [
+     //堂食OC
+
+    'eat_oc_db_connect'=>[
+        'db_host'=>'sm12g5l9d32eyun.cjwa2zciaejp.rds.cn-north-1.amazonaws.com.cn',
+        'db_name'=>'keithyau',
+        'db_pass'=>'thomas123',
+        'db_database'=>'syncedoc',
+     ],
+     //外卖oc
+
+     'takeout_oc_db_connect'=>[
+         'db_host'=>'sm12g5l9d32eyun.cjwa2zciaejp.rds.cn-north-1.amazonaws.com.cn',
+         'db_name'=>'keithyau',
+         'db_pass'=>'thomas123',
+         'db_database'=>'gfsoc',
+     ],
+
+     //ERP
+
+    'erp_db_connect'=>[
+        'db_host'=>'sm12g5l9d32eyun.cjwa2zciaejp.rds.cn-north-1.amazonaws.com.cn',
+        'db_name'=>'keithyau',
+        'db_pass'=>'thomas123',
+        'db_database'=>'erp',
+    ],
+
+     //本地服务器DB 用于多库联查
+     'loc_db_connect'=>[
+         'db_host'=>'sm12g5l9d32eyun.cjwa2zciaejp.rds.cn-north-1.amazonaws.com.cn',
+         'db_name'=>'keithyau',
+         'db_pass'=>'thomas123',
+         'db_database'=>'oc',
+     ],
+
+];
+
+
+/**
+ * 设置多库联查的库名称
+ */
+$GLOBALS['dbname'] =[
+    'oc'=>'oc',      //设置oc的数据库名称
+    'erp'=>'erp',              //设置erp的数据库名称
+    'laravel'=> 'laravel',
+    'de'=>'de',
+    'crm'=>'crm'
+];
+
