0c600fa0d053ecff6178d7509a484cddd2dec010
Jinjin123 <1293813551@qq.com>
2017-02-23 01:42:33 +0800

    Updating cookbook of drupalsetting to use appname as variable

--- cookbooks/drupalsetting/recipes/default.rb
+++ cookbooks/drupalsetting/recipes/default.rb
@@ -42,7 +42,7 @@ node[:deploycode][:configuration].each do |appname,spec|
         :host => dbhost,
         :username => dbuser,
         :password => dbpass,
-        :dbname => dir
+        :dbname => appname
       )
         #The filename already in template folder
         source sourcefile
