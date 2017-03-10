bd02128e7d5688dd4a20bd84535a64b864a49a4a
Jinjin123 <1293813551@qq.com>
2017-03-11 00:06:46 +0800

    Remove proxyport entry from cookbook

--- cookbooks/webserver/recipes/default.rb
+++ cookbooks/webserver/recipes/default.rb
@@ -209,7 +209,6 @@ node[:deploycode][:runtime].each do |localfolder,docker|
         template "#{node[:deploycode][:basedirectory]}bootproxy/#{localfolder}.proxy.conf" do
           variables(
             :host => container_name,
-            :portstring => portstring,
             :domain  => domainstring
           )
           source "proxy.conf"
