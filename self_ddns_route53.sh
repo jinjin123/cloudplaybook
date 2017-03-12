0e476e1378af4c4b99845499b4923985f71411bd
root <root@localhost.localdomain>
2017-03-12 06:26:22 -0400

    add env

--- self_ddns_route53.sh
+++ self_ddns_route53.sh
@@ -1,11 +1,16 @@
 #! /bin/bash
 
+cd /root/bootdev/chef/chef-repo
+
 ownip=`dig +short myip.opendns.com @resolver1.opendns.com`
 
 thisuniqueid="$(dmidecode | grep -i uuid | head -1 | awk -F" " '{print $2}')"
 
 #Update the Route53 cookbook self name for dockers to CNAME
 changeme=`echo -n ${thisuniqueid} | md5sum | awk -F" " '{ print $1 }'`
+
+echo $changeme
+
 cat <<EOF  > user_jsons/self_domain_cron.json
 {
       "deployuser":"root",
