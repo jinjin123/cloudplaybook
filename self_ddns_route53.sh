5d2e63221bb5e880ed2fc05fb9513053649990f3
root <root@localhost.localdomain>
2017-03-12 06:22:36 -0400

    remove dynamic create files and add Route53 as DDNS, change the unique identifier for the host base route53 record

---
+++ self_ddns_route53.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+
+ownip=`dig +short myip.opendns.com @resolver1.opendns.com`
+
+thisuniqueid="$(dmidecode | grep -i uuid | head -1 | awk -F" " '{print $2}')"
+
+#Update the Route53 cookbook self name for dockers to CNAME
+changeme=`echo -n ${thisuniqueid} | md5sum | awk -F" " '{ print $1 }'`
+cat <<EOF  > user_jsons/self_domain_cron.json
+{
+      "deployuser":"root",
+      "externalmode":"normal",
+      "projectname":"shadowdock",
+      "domainname":"shadowdock.com",
+      "domain_type":"A",
+      "domainprefix":"${changeme}",
+      "thisserver":"${ownip}",
+      "deploycode":{
+         "basedirectory":"/home/keithyau/bootdev/shadowdock/",
+         "runtime":{
+            "bootspotowndomain":{
+               "tag":"latest",
+               "image":"spotdocker",
+               "env":[
+                  "DUMMY"
+               ],
+               "command":"",
+               "mountlocal":"localdir",
+               "mountdocker":"/mnt",
+               "proxyport":"80",
+               "ports":"80"
+            }
+        }
+      }
+}
+EOF
+
+chef-solo -c ./settings/solo.rb -o "recIpe[bootdev_customdomain]" -j user_jsons/self_domain_cron.json
