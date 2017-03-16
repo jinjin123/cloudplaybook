12c88bb2490401c6c398278028cc73a1644c303b
Jinjin123 <1293813551@qq.com>
2017-03-16 18:15:49 +0800

    Creating function to login azure from docker

--- cookbooks/azure/recipes/kylin.rb
+++ cookbooks/azure/recipes/kylin.rb
@@ -9,3 +9,13 @@
 
 credentials = node[:deploycode][:configuration][:azure][:credentials]
 image = node[:deploycode][:runtime][:azure-cli][:image]
+
+if (not (defined?(credentials[:env])).nil?)
+	execute 'login_china' do
+	  command "docker run -it #{image} \'azure login --username #{credentials[:username]} --password #{credentials[:password]} --environment #{credentials[:env]}\'"
+	end
+else
+	execute 'login_global' do
+	  command "docker run -it #{image} \'azure login --username #{credentials[:username]} --password #{credentials[:password]}\'"
+	end
+end
