7c2fca346f8678edbe0cfd63770ac85bde54ffdb
Jinjin123 <1293813551@qq.com>
2017-03-16 07:19:00 +0000

    Adding azure cookbook

---
+++ cookbooks/azure/recipes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: azure
+# Recipe:: default
+#
+# Copyright 2017, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+if (not (defined?(node[:deploycode][:configuration][:azure][:kylin])).nil?)
+  include_recipe '::kylin.rb'
+end
