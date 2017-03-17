bf0c8f4680c4ed6521fb32a3c1cbe17d0bc1a71e
Jinjin123 <1293813551@qq.com>
2017-03-17 13:49:57 +0800

    Providing name to docker container for azure

--- cookbooks/azure/recipes/kylin.rb
+++ cookbooks/azure/recipes/kylin.rb
@@ -9,13 +9,16 @@
 
 credentials = node[:deploycode][:configuration][:azure][:credentials]
 image = node[:deploycode][:runtime]["azure-cli"][:image]
+# Name of docker container is not imaport, just make one
+container_name = "azure"
 
 if (not (defined?(credentials[:env])).nil?)
 	execute 'login_china' do
-	  command "docker run -it #{image} \'azure login --username #{credentials[:username]} --password #{credentials[:password]} --environment #{credentials[:env]}\'"
+	  command "docker run #{image} --name #{container_name} \'azure login --username #{credentials[:username]} --password #{credentials[:password]} --environment #{credentials[:env]}\'"
 	end
 else
 	execute 'login_global' do
-	  command "docker run -it #{image} \'azure login --username #{credentials[:username]} --password #{credentials[:password]}\'"
+	  command "docker run #{image} --name #{container_name} \'azure login --username #{credentials[:username]} --password #{credentials[:password]}\'"
 	end
 end
+
