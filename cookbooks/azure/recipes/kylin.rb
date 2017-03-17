508d1827f70df7b28d490cc02c7b11918b5bb448
Jinjin123 <1293813551@qq.com>
2017-03-17 15:37:20 +0800

    Updating azure docker to reinit

--- cookbooks/azure/recipes/kylin.rb
+++ cookbooks/azure/recipes/kylin.rb
@@ -12,19 +12,20 @@ image = node[:deploycode][:runtime]["azure-cli"][:image]
 # Name of docker container is not imaport, just make one
 container_name = "azure"
 
+# Reinit azure docker_container
 if (not (defined?(credentials[:env])).nil?)
 	execute 'login_china' do
-	  command "
-	    $VAR=`docker ps -a| awk {\'print $(NF)\'}| grep \'^#{container_name}$\'|wc -l`;
-	    if [ $VAR -eq \"1\" ]; 
-	    then docker run --name #{container_name} #{image} azure login --username #{credentials[:username]} --password #{credentials[:password]} --environment #{credentials[:env]}
-        else docker run #{image} azure login --username #{credentials[:username]} --password #{credentials[:password]} --environment #{credentials[:env]}
-        fi
-	    "
+	  command "docker rm #{container_name};docker run --name #{container_name} #{image} azure login --username #{credentials[:username]} --password #{credentials[:password]} --environment #{credentials[:env]}"
+      notifies :run, 'execute[commit_docker]', :immediately
 	end
 else
 	execute 'login_global' do
-	  command "docker run --name #{container_name} #{image} azure login --username #{credentials[:username]} --password #{credentials[:password]}"
+	  command "docker rm #{container_name};docker run --name #{container_name} #{image} azure login --username #{credentials[:username]} --password #{credentials[:password]}"
+      notifies :run, 'execute[commit_docker]', :immediately
 	end
 end
 
+execute "commit_docker" do
+	command "docker commit #{container_name} #{container_name}_tmp;docker rm #{container_name};docker commit #{container_name}_tmp #{container_name};docker rm #{container_name}_tmp"
+    action :nothing
+end
