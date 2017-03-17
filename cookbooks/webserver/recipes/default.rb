91e203872aa862aa2c960c64ae3deb8344223743
Jinjin123 <1293813551@qq.com>
2017-03-17 15:43:47 +0800

    Revert "Not running runtime if not defined"
    This reverts commit 888b9ea6a7dea07f1d66bee1db72cc8424b11639.

--- cookbooks/webserver/recipes/default.rb
+++ cookbooks/webserver/recipes/default.rb
@@ -90,216 +90,214 @@ if (not (defined?(node[:docker][:privaterepo])).nil?) && (not "#{node[:docker][:
   end
 end
 
-if (not (defined?(node[:deploycode][:runtime])).nil?)
-  #todo make the array be unique elements
-  node[:deploycode][:runtime].each do |localfolder,docker|
-    # Pull latest image
-    docker_image docker[:image] do
-      tag docker[:tag]
-      action :pull
-  #   notifies :redeploy, 'docker_container[webservice]'
-    end
+#todo make the array be unique elements
+node[:deploycode][:runtime].each do |localfolder,docker|
+  # Pull latest image
+  docker_image docker[:image] do
+    tag docker[:tag]
+    action :pull
+#   notifies :redeploy, 'docker_container[webservice]'
   end
+end
 
-  etchosts = []
-  if (not (defined?(node[:deploycode][:runtime])).nil?)
-    node[:deploycode][:runtime].each do |localfolder,docker|
-      #if tagged localdir, give the localfolder as mount point 
-      if (not (defined?(docker[:mountlocal])).nil?)
-        if docker[:mountlocal].eql?("localdir")
-          #Override dir to custom url
-          dir_name = "#{node[:deploycode][:basedirectory]}#{localfolder}"
-          node.default["bindvolume"] = [ "#{node[:deploycode][:basedirectory]}#{localfolder}:#{docker[:mountdocker]}" ]
+etchosts = []
+if (not (defined?(node[:deploycode][:runtime])).nil?)
+  node[:deploycode][:runtime].each do |localfolder,docker|
+    #if tagged localdir, give the localfolder as mount point 
+    if (not (defined?(docker[:mountlocal])).nil?)
+      if docker[:mountlocal].eql?("localdir")
+        #Override dir to custom url
+        dir_name = "#{node[:deploycode][:basedirectory]}#{localfolder}"
+        node.default["bindvolume"] = [ "#{node[:deploycode][:basedirectory]}#{localfolder}:#{docker[:mountdocker]}" ]
+      else
+        if docker[:mountlocal].eql?("multipledir")
+          node.default["bindvolume"] = docker[:mountdocker]
         else
-          if docker[:mountlocal].eql?("multipledir")
-            node.default["bindvolume"] = docker[:mountdocker]
-          else
-            dir_name = docker[:mountlocal]
-            node.default["bindvolume"] = [ "#{docker[:mountlocal]}:#{docker[:mountdocker]}" ]
-          end
-        end
-
-        if not docker[:mountlocal].eql?("multipledir") && (not (defined?(dir_name)).nil?)
-          if localfolder.eql?("mysql")
-            dir_permission = '0777'
-          else 
-            dir_permission = '0755'
-          end
-          #Prepare directories
-          directory "#{dir_name}" do
-            owner user
-            group user
-            mode "#{dir_permission}"
-            recursive true
-            ignore_failure true
-            action :create
-          end
-        end
-      end
-      
-      if (not (defined?(node[:deploycode][:configuration][:general][localfolder])).nil?) && (not "#{node[:deploycode][:configuration][:general]}" == "")
-        spec = node[:deploycode][:configuration][:general]["#{localfolder}"]
-        spec.each do |file,path|
-          node.default["bindvolume"].push("#{basedir}#{localfolder}_configuration/#{file}:#{path}")
+          dir_name = docker[:mountlocal]
+          node.default["bindvolume"] = [ "#{docker[:mountlocal]}:#{docker[:mountdocker]}" ]
         end
       end
 
-      # Begin running containers
-      container_name = "#{node[:projectname]}_" + localfolder
-      if container_name.eql?("#{node[:projectname]}_mysql") 
-        #Add the first docker
+      if not docker[:mountlocal].eql?("multipledir") && (not (defined?(dir_name)).nil?)
+        if localfolder.eql?("mysql")
+          dir_permission = '0777'
+        else 
+          dir_permission = '0755'
+        end
+        #Prepare directories
+        directory "#{dir_name}" do
+          owner user
+          group user
+          mode "#{dir_permission}"
+          recursive true
+          ignore_failure true
+          action :create
+        end
+      end
+    end
+    
+    if (not (defined?(node[:deploycode][:configuration][:general][localfolder])).nil?) && (not "#{node[:deploycode][:configuration][:general]}" == "")
+      spec = node[:deploycode][:configuration][:general]["#{localfolder}"]
+      spec.each do |file,path|
+        node.default["bindvolume"].push("#{basedir}#{localfolder}_configuration/#{file}:#{path}")
+      end
+    end
+
+    # Begin running containers
+    container_name = "#{node[:projectname]}_" + localfolder
+    if container_name.eql?("#{node[:projectname]}_mysql") 
+      #Add the first docker
+      docker_container container_name do
+        repo docker[:image]
+        tag docker[:tag]
+        kill_after 3
+        env docker[:env]
+        command '--sql-mode="ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION"'
+        action :run
+        #ignore_failure true
+        port docker[:ports]
+        volumes node.default["bindvolume"]
+      end
+      etchosts.push("#{container_name}:#{container_name}")
+      #Break and dont create mysql proxy.conf
+      next
+    else 
+      #Special handling if bootproxy,  get all local running docker id and name and link into bootproxy
+      if localfolder.eql?("bootproxy")
+        node.set[:dockerinfo] = []
+        results = "/tmp/dockerinfo.txt"
+        file results do
+          action :delete
+        end
+
+        cmd = "docker ps -a|grep -v CONTAINER|grep -v monitor|awk \'{print $1, $NF}\'"
+        bash cmd do
+          code <<-EOH
+          #{cmd} &> #{results}
+          EOH
+        end
+        ruby_block "Results" do
+          only_if { ::File.exists?(results) }
+          block do
+            f = File.open(results)
+            dockerinfo = Hash.new 
+            f.each do |line|
+              dockerinfo[line.chomp.split(' ')[0]] = line.chomp.split(' ')[1]
+            end
+            f.close
+            node.set[:dockerinfo] = dockerinfo
+            node.run_state[:linking] = dockerinfo
+            node.set[:linking] = []
+            node.set[:dockerinfo].each do |hash, dockername|
+              node.set[:linking].push("#{dockername}:#{dockername}")
+            end
+          end
+        end
+      else
+        node.set[:linking] = etchosts
+      end
+
+      if node.default["bindvolume"].eql?([":"])
+        node.default["bindvolume"] = nil
+      end
+      if localfolder.eql?("bootproxy")
+        # Using lazy evaluation if bootproxy
         docker_container container_name do
           repo docker[:image]
           tag docker[:tag]
-          kill_after 3
+          #Add all docker link
+          links lazy{node.set[:linking]}
           env docker[:env]
-          command '--sql-mode="ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION"'
+          command docker[:command]
+          kill_after 30
+    #      autoremove true
           action :run
-          #ignore_failure true
           port docker[:ports]
           volumes node.default["bindvolume"]
+          cap_add 'SYS_ADMIN' 
+          devices []
+          privileged true 
+          timeout 30
+    #      {["/dev/fuse"]}
         end
-        etchosts.push("#{container_name}:#{container_name}")
-        #Break and dont create mysql proxy.conf
-        next
-      else 
-        #Special handling if bootproxy,  get all local running docker id and name and link into bootproxy
-        if localfolder.eql?("bootproxy")
-          node.set[:dockerinfo] = []
-          results = "/tmp/dockerinfo.txt"
-          file results do
-            action :delete
-          end
+      else
+        docker_container container_name do
+          repo docker[:image]
+          tag docker[:tag]
+          #Add all docker link
+          links node.set[:linking]
+          env docker[:env]
+          command docker[:command]
+          kill_after 30
+    #      autoremove true
+          action :run
+          port docker[:ports]
+          volumes node.default["bindvolume"]
+          cap_add 'SYS_ADMIN' 
+          devices []
+          privileged true 
+          timeout 30
+    #      {["/dev/fuse"]}
+        end
+      end
 
-          cmd = "docker ps -a|grep -v CONTAINER|grep -v monitor|awk \'{print $1, $NF}\'"
-          bash cmd do
-            code <<-EOH
-            #{cmd} &> #{results}
-            EOH
-          end
-          ruby_block "Results" do
-            only_if { ::File.exists?(results) }
-            block do
-              f = File.open(results)
-              dockerinfo = Hash.new 
-              f.each do |line|
-                dockerinfo[line.chomp.split(' ')[0]] = line.chomp.split(' ')[1]
-              end
-              f.close
-              node.set[:dockerinfo] = dockerinfo
-              node.run_state[:linking] = dockerinfo
-              node.set[:linking] = []
-              node.set[:dockerinfo].each do |hash, dockername|
-                node.set[:linking].push("#{dockername}:#{dockername}")
-              end
-            end
-          end
-        else
-          node.set[:linking] = etchosts
+      if (not (defined?(docker[:exec])).nil?) && (not "#{docker[:exec]}" == "")
+        execute 'execute command inside docker' do
+        command "docker exec -i #{container_name} /bin/bash -c \'#{docker[:exec]}\'"
         end
+      end
 
-        if node.default["bindvolume"].eql?([":"])
-          node.default["bindvolume"] = nil
-        end
-        if localfolder.eql?("bootproxy")
-          # Using lazy evaluation if bootproxy
-          docker_container container_name do
-            repo docker[:image]
-            tag docker[:tag]
-            #Add all docker link
-            links lazy{node.set[:linking]}
-            env docker[:env]
-            command docker[:command]
-            kill_after 30
-      #      autoremove true
-            action :run
-            port docker[:ports]
-            volumes node.default["bindvolume"]
-            cap_add 'SYS_ADMIN' 
-            devices []
-            privileged true 
-            timeout 30
-      #      {["/dev/fuse"]}
-          end
-        else
-          docker_container container_name do
-            repo docker[:image]
-            tag docker[:tag]
-            #Add all docker link
-            links node.set[:linking]
-            env docker[:env]
-            command docker[:command]
-            kill_after 30
-      #      autoremove true
-            action :run
-            port docker[:ports]
-            volumes node.default["bindvolume"]
-            cap_add 'SYS_ADMIN' 
-            devices []
-            privileged true 
-            timeout 30
-      #      {["/dev/fuse"]}
-          end
-        end
-
-        if (not (defined?(docker[:exec])).nil?) && (not "#{docker[:exec]}" == "")
-          execute 'execute command inside docker' do
-          command "docker exec -i #{container_name} /bin/bash -c \'#{docker[:exec]}\'"
-          end
-        end
-
-        etchosts.push("#{container_name}:#{container_name}")
+      etchosts.push("#{container_name}:#{container_name}")
+    end
+   
+    #Add proxy.conf to folder if bootproxy defined
+    if defined?(node[:externalmode]) && node[:externalmode].eql?("bootproxy")
+      #Prepare bootproxy directories
+      directory "#{node[:deploycode][:basedirectory]}bootproxy" do
+        owner user
+        group user
+        mode '0755'
+        recursive true
+        action :create
       end
      
-      #Add proxy.conf to folder if bootproxy defined
-      if defined?(node[:externalmode]) && node[:externalmode].eql?("bootproxy")
-        #Prepare bootproxy directories
-        directory "#{node[:deploycode][:basedirectory]}bootproxy" do
-          owner user
-          group user
-          mode '0755'
-          recursive true
-          action :create
-        end
-       
-        # if docker[:proxyport].eql?("80")
-        #   portstring = ""
-        # else
-        #   portstring = ":#{docker[:proxyport]}"
-        # end
+      # if docker[:proxyport].eql?("80")
+      #   portstring = ""
+      # else
+      #   portstring = ":#{docker[:proxyport]}"
+      # end
 
-        #Skip template create for bootdev proxy
-        next if localfolder.eql?("bootproxy")
-        domainprefixset = node[:domainprefix]
-        if (not (defined?(docker[:customdomainprefix])).nil?) && (not "#{docker[:customdomainprefix]}" == "")
-          domainprefixset = docker[:customdomainprefix]
-        end
-          if (not (defined?(docker[:overridesubdomain])).nil?) && (not "#{docker[:overridesubdomain]}" == "")
-            if docker[:overridesubdomain].eql?("www")
-              domainstring = "#{docker[:overridesubdomain]}.#{node[:domainname]} #{node[:domainname]}"
-            else
-              domainstring = "#{docker[:overridesubdomain]}.#{node[:domainname]}"
-            end
-          else
-            domainstring = "#{domainprefixset}#{localfolder}.#{node[:domainname]}"
-          end
-          template "#{node[:deploycode][:basedirectory]}bootproxy/#{localfolder}.proxy.conf" do
-            variables(
-              :host => container_name,
-              :domain  => domainstring,
-              :proxyport => "80"
-            )
-            source "proxy.conf"
-            mode 0644
-            retries 3
-            retry_delay 2
-            owner "root"
-            group "root"
-            action :create
-        #    ignore_failure true
-          end
-        #end
+      #Skip template create for bootdev proxy
+      next if localfolder.eql?("bootproxy")
+      domainprefixset = node[:domainprefix]
+      if (not (defined?(docker[:customdomainprefix])).nil?) && (not "#{docker[:customdomainprefix]}" == "")
+        domainprefixset = docker[:customdomainprefix]
       end
+        if (not (defined?(docker[:overridesubdomain])).nil?) && (not "#{docker[:overridesubdomain]}" == "")
+          if docker[:overridesubdomain].eql?("www")
+            domainstring = "#{docker[:overridesubdomain]}.#{node[:domainname]} #{node[:domainname]}"
+          else
+            domainstring = "#{docker[:overridesubdomain]}.#{node[:domainname]}"
+          end
+        else
+          domainstring = "#{domainprefixset}#{localfolder}.#{node[:domainname]}"
+        end
+        template "#{node[:deploycode][:basedirectory]}bootproxy/#{localfolder}.proxy.conf" do
+          variables(
+            :host => container_name,
+            :domain  => domainstring,
+            :proxyport => "80"
+          )
+          source "proxy.conf"
+          mode 0644
+          retries 3
+          retry_delay 2
+          owner "root"
+          group "root"
+          action :create
+      #    ignore_failure true
+        end
+      #end
     end
   end
 end
