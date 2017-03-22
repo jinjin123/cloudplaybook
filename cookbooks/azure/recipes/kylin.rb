ead9f6bc6e4bc6e9eb6542937d7462d210fd1478
Jinjin123 <1293813551@qq.com>
2017-03-22 13:15:17 +0800

    Deploying template of azure

--- cookbooks/azure/recipes/kylin.rb
+++ cookbooks/azure/recipes/kylin.rb
@@ -20,6 +20,14 @@ credentials = node[:deploycode][:configuration][:azure][:credentials]
 container_name = "#{node[:projectname]}_azure"
 # Aggregating operations into image, default = container_name
 image_name = container_name
+# Setting basedir to store template files
+basedir = node[:deploycode][:basedirectory]
+username = node[:deployuser]
+
+# storing kylin variables to be called
+if (not (defined?(node[:deploycode][:configuration][:azure][:kylin])).nil?) && (not "#{node[:deploycode][:configuration][:azure][:kylin]}" == "")
+  kylin = node[:deploycode][:configuration][:azure][:kylin]
+end
 
 execute "createimageifnotexist_removecontainerifexist" do
     command "if [ `docker images|awk {'print $NF'}|grep \'^#{image_name}$\'|wc -l` != \'1\' ];then docker commit #{container_name} #{image_name};fi;if [ `docker ps -a|awk {'print $NF'}|grep \'^#{container_name}$\'|wc -l` == \'1\' ];then docker stop #{container_name}||true;docker rm #{container_name}||true;fi"
@@ -41,4 +49,36 @@ end
 execute "commit_docker" do
 	command "docker commit #{container_name} #{image_name}_tmp;docker rm #{container_name};docker rmi #{image_name};docker tag #{image_name}_tmp #{image_name};docker rmi #{image_name}_tmp"
     action :nothing
+end
+
+if (not (defined?(kylin)).nil?) && (not "#{kylin}" == "")
+identifier = kylin[:identifier]
+  directory "#{basedir}azure/#{identifier}" do
+    owner username
+    group username
+    mode '0755'
+    recursive true
+    action :create
+  end
+  end
+  if kylin[:region].downcase.include?("china")
+    template "#{basedir}azure/#{identifier}/deploywithcluster_cn.#{identifier}.json"
+      source "deploywithcluster_cn.json"
+      mode 0644
+      retries 3
+      retry_delay 2
+      owner "root"
+      group "root"
+      action :create
+    end
+  else
+    template "#{basedir}azure/#{identifier}/deploywithcluster.#{identifier}.json"
+      source "deploywithcluster.json"
+      mode 0644
+      retries 3
+      retry_delay 2
+      owner "root"
+      group "root"
+      action :create
+  end
 end
