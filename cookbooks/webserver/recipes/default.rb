d0f0c4509e749d73267d4e3cb96d802c60000aee
Jinjin123 <1293813551@qq.com>
2017-03-23 13:37:11 +0800

    Setting memeory for docker_container

--- cookbooks/webserver/recipes/default.rb
+++ cookbooks/webserver/recipes/default.rb
@@ -212,6 +212,11 @@ if (not (defined?(node[:deploycode][:runtime])).nil?) && (not "#{node[:deploycod
       if node.default["bindvolume"].eql?([":"])
         node.default["bindvolume"] = nil
       end
+      if (not (defined?(docker[:memory_limit])).nil?) && (not "#{docker[:memory_limit]}" == "")
+        memory_limit = docker[:memory_limit]
+      else
+        memory_limit = "-1"
+      end
       if localfolder.eql?("bootproxy")
         # Using lazy evaluation if bootproxy
         docker_container container_name do
@@ -231,6 +236,7 @@ if (not (defined?(node[:deploycode][:runtime])).nil?) && (not "#{node[:deploycod
           devices []
           privileged true
           timeout 30
+          memory memory_limit
     #      {["/dev/fuse"]}
         end
       else
@@ -250,6 +256,7 @@ if (not (defined?(node[:deploycode][:runtime])).nil?) && (not "#{node[:deploycod
           devices []
           privileged true
           timeout 30
+          memory memory_limit
     #      {["/dev/fuse"]}
         end
       end
