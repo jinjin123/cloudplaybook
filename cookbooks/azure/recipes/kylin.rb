f10da0dbbae76531d4e4195a684ae5f6a68005a0
Jinjin123 <1293813551@qq.com>
2017-03-24 11:07:16 +0800

    putting storageAccount name to use identifier id

--- cookbooks/azure/recipes/kylin.rb
+++ cookbooks/azure/recipes/kylin.rb
@@ -91,7 +91,7 @@ if (not (defined?(kylin)).nil?) && (not "#{kylin}" == "")
         :metastoreName => kylin[:metastoreName],
         :sshUserName => kylin[:sshUserName],
         :sshPassword => kylin[:sshPassword],
-        :storageAccount => kylin[:storageAccount]
+        :storageAccount => "#{kylin[:identifier]}storageAccount"
       )
       mode 0644
       retries 3
