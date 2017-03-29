1dda84e68ac5306f4275b2c79037f4a279b5311b
Jinjin123 <1293813551@qq.com>
2017-03-29 17:01:30 +0800

    updating de.setting.php for sparkpad-dev

--- cookbooks/drupalsetting/templates/default/sparkpad-dev-de.settings.php
+++ cookbooks/drupalsetting/templates/default/sparkpad-dev-de.settings.php
@@ -26,3 +26,4 @@ $conf['404_fast_paths'] = '/\.(?:txt|png|gif|jpe?g|css|js|ico|swf|flv|cgi|bat|pl
 $conf['404_fast_html'] = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN" "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd"><html xmlns="http://www.w3.org/1999/xhtml"><head><title>404 Not Found</title></head><body><h1>Not Found</h1><p>The requested URL "@path" was not found on this server.</p></body></html>';
 $conf['cache_class_cache_form'] = 'DrupalDatabaseCache';
 $conf['restws_basic_auth_user_regex'] = '/.*/';
+$conf['user_failed_login_ip_limit'] = 1000;
