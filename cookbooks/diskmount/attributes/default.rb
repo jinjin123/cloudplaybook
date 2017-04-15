8678f26e2e5105e26dd49becedd3e16556265163
Jinjin123 <1293813551@qq.com>
2017-04-15 05:09:51 -0400

    Inserting proddrupal role back

--- cookbooks/diskmount/attributes/default.rb
+++ cookbooks/diskmount/attributes/default.rb
@@ -1,4 +1,4 @@
 # gluster fs mount config
-node[:diskmount][:glusterserverip] = '10.0.0.0'
-node[:diskmount][:glustervolume] = 'glt0'
-node[:diskmount][:localsourcefolder] = '/var/www/shares/'
+default[:diskmount][:glusterserverip] = '10.0.0.0'
+default[:diskmount][:glustervolume] = 'glt0'
+default[:diskmount][:localsourcefolder] = '/var/www/shares/'
