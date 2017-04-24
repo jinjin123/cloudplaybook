a19ff51c3c082c76c47da7b8870f623ca1a1cf98
Jinjin123 <1293813551@qq.com>
2017-04-24 14:22:31 +0800

    upgrade  production settings.php

--- cookbooks/drupalsetting/templates/default/spzkf_pmt.settings.php
+++ cookbooks/drupalsetting/templates/default/spzkf_pmt.settings.php
@@ -40,3 +40,42 @@ $conf['memcache_key_prefix'] = 'pmt';
 
 ini_set('display_errors', TRUE);
 ini_set('display_startup_errors', TRUE);
+
+$conf['zkf_api'] = array(
+  'oc' => array(
+    'api_url' => 'http://172.16.104.124:8080/oc/',
+    #'api_url' => 'http://172.16.102.116:8080/',
+    'api_key' => 'CCSERVER',
+    'api_secret' => 'CCSERVER3345',
+    'fixed_access_token' => '481A0y0722G721S5p21853gNNnc583N9DUZ4f90995lQbl6pGw229zA1652P2h56',
+    'enable_log' => 1,
+  ),
+  'de' => array(
+    'api_url' => 'http://zkf_de/',
+    'api_key' => 'bPOf3a52dcWw6vkE7QtgVoRUz',
+    'enable_log' => 0,
+  ),
+  'crm' => array(
+    'api_url' => 'http://zkf_crm/',
+    'api_key' => 'W/bkcCjGNmzjS/l6DTwxDA==',
+    'enable_log' => 1,
+  ),
+  'marketing' => array(
+    'api_url' => 'http://zkf_mkt/',
+    'api_key' => 'zkf_ts',
+    'api_secret' => 'zkf_ts_secret',
+    'discount_id' => 59,
+    'enable_log' => 1,
+  ),
+  'payment' => array(
+    'api_url' => 'http://zkf_pmt/',
+    'api_key' => '8a970a5f2835ad6ba323db0e73aee6f1',
+    'api_security' => '369c9360b966d3a4e4154fbc96efe0f0',
+    'enable_log' => 1,
+  ),
+  'baidu_map' => array(
+    'api_url' => 'http://api.map.baidu.com/',
+    'api_key' => '8c1b6507d5db2db7aca15cd249fdd385',
+    'enable_log' => 1,
+  ),
+);
