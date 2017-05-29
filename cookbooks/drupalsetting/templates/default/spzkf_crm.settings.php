2033a296f40f1f8ac65ae5dd30653c5cacda1063
Jinjin123 <1293813551@qq.com>
2017-05-29 16:53:37 +0800

    Updating crm database credentials in crm.settings.php

--- cookbooks/drupalsetting/templates/default/spzkf_crm.settings.php
+++ cookbooks/drupalsetting/templates/default/spzkf_crm.settings.php
@@ -4,9 +4,9 @@ $databases = array (
   'default' => array (
     'default' => array (
       'database' => 'crm',
-      'username' => "<%= @dbuser %>",
-      'password' => "<%= @dbpass %>",
-      'host' => "<%= @dbhost %>",
+      'username' => "crm",
+      'password' => "?*;^1T/%XyJA;W!rED3H",
+      'host' => "172.16.103.121",
       'port' => '',
       'driver' => 'mysql',
       'prefix' => '',
