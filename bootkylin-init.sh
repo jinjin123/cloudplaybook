4358a5fe8e54e7b39b6ca11159881eb44ac8cdd8
root <root@ip-10-0-0-188.us-west-2.compute.internal>
2017-06-02 06:19:28 +0000

    update for aws init commit

---
+++ bootkylin-init.sh
@@ -0,0 +1,194 @@
+#! /bin/sh
+
+#Disable SELinux, otherwise, docker mount with permission fail
+echo "SELINUX=disabled" > /etc/sysconfig/selinux
+setenforce 0
+
+# Sync time
+CHECKING_NTPDATE=`command -v ntpdate|wc -l`
+if [ "$CHECKING_NTPDATE" != "0" ]; then
+    echo "NTPDATE exists"
+else
+    yum -y install ntp ntpdate ntp-doc
+fi
+ntpdate pool.ntp.org
+
+#Assume Centos and login as root
+cd /root
+
+#Install Chef-solo
+CHECKING_CHEFSOLO=`command -v chef-solo|wc -l`
+if [ "$CHECKING_CHEFSOLO" != "0" ]; then
+    echo "Chef Solo exists"
+else
+    echo "Installing Chef Solo"
+    /usr/bin/curl -L https://www.opscode.com/chef/install.sh | bash
+fi
+
+#Create Chef-repo
+mkdir -p /root/kylin/chef/chef-repo
+#just in case
+#mkdir -p /home/keithyau/bootdev/shadowdock/
+cd /root/kylin/chef/chef-repo
+
+#curl upgrade
+yum -y update
+CHECKING_GIT=`command -v git|wc -l`
+if [ "$CHECKING_GIT" != "0" ]; then
+    echo "GIT exists"
+else
+    yum -y install git
+fi
+
+#Must install
+yum -y install curl wget bind-utils
+
+#checkout working branch
+git clone -b docker-general-kyligence https://keithyau:thomas123@github.com/Kyligence/kylindeploy.git .
+
+#prepare directories
+mkdir -p logs
+
+#Update this server password: ToDo Random a password
+#echo 'thomas1234!' | passwd root --stdin
+
+#Make this Linux Unique ID
+#thisuniqueid="$(dmidecode | grep -i uuid | head -1 | awk -F" " '{print $2}')"
+#Since dmidecode code cannot get unique id, temp use mac address
+#thisuniqueid=`ip addr show | grep ether | head -1 | awk -F" "  '{print $2}'`
+
+#get own IP and register to laravel, tell Laravel this server's password
+#ownip=`/usr/bin/curl --user "keithyau@163.com":thomas123 -F "linux_uid=${thisuniqueid}" -F 'spot_sshpass=thomas1234!' -F "spot_sshroot=root" http://d.bootdev.com/spot-register`
+
+#Update the Route53 cookbook self name for dockers to CNAME
+changeme=`echo -n ${thisuniqueid} | md5sum | awk -F" " '{ print $1 }'`
+cat <<EOF  > user_jsons/self_domain.json
+{
+      "deployuser":"root",
+      "externalmode":"normal",
+      "projectname":"shadowdock",
+      "domainname":"shadowdock.com",
+      "domain_type":"A",
+      "domainprefix":"${changeme}",
+      "thisserver":"${ownip}",
+      "deploycode":{
+         "basedirectory":"/home/keithyau/bootdev/shadowdock/",
+         "runtime":{  
+            "bootspotowndomain":{  
+               "tag":"latest",
+               "image":"spotdocker",
+               "env":[  
+                  "DUMMY"
+               ],
+               "command":"",
+               "mountlocal":"localdir",
+               "mountdocker":"/mnt",
+               "proxyport":"80",
+               "ports":"80"
+            }
+        }
+      }
+}
+EOF
+
+#chef-solo -c ./settings/solo.rb -o "recipe[bootdev_customdomain]" -j user_jsons/self_domain.json
+
+
+#Save the new name into role, replace the default, put already cname domain in it
+cat <<EOF  > roles/chefsoloinit.json
+{
+   "name":"chefsoloinit",
+   "description":"this add a single new user docker to shadowdock",
+   "json_class":"Chef::Role",
+   "default_attributes":{
+      "deployuser":"root",
+      "externalmode":"bootproxy",
+      "projectname":"shadowdock",
+      "domainname":"shadowdock.com",
+      "thisserver":"${changeme}.shadowdock.com",
+      "docker": {
+          "privaterepo":"dockerpriv.shadowdock.com",
+          "username":"keithyau",
+          "password":"thomas123"
+      },
+      "deploycode":{
+         "basedirectory":"/home/keithyau/bootdev/shadowdock/",
+         "localfolder":{  
+            "shadowsocksinit":"nodownload"
+         },
+         "runtime":{
+            "shadowsocksinit":{
+               "tag":"latest",
+               "image":"keithyau/ss",
+               "env":[
+                  "DUMMY"
+               ],
+               "command":"-k thomas123 -w 2 -f",
+               "mountlocal":"localdir",
+               "mountdocker":"/mnt",
+               "proxyport":"444",
+               "ports":"5000"
+            }
+        }
+      }
+   },
+   "override_attributes":{
+
+   },
+   "chef_type":"role",
+   "run_list":[
+      "recipe[git]",
+      "recipe[build-essential]",
+      "recipe[basicservice]",
+      "recipe[glusterfs]",
+      "recipe[webserver]",
+      "recipe[deploycode]"
+   ],
+   "env_run_lists":{
+
+   }
+}
+EOF
+
+#Generate required role per docker run
+cat <<EOF > roles/singleusercreate.json
+{
+   "name":"singleusercreate",
+   "description":"this add a single new user docker to shadowdock",
+   "json_class":"Chef::Role",
+   "default_attributes":{
+      "deployuser":"root",
+      "externalmode":"bootproxy",
+      "projectname":"shadowdock",
+      "domainname":"shadowdock.com",
+      "domain_type":"A",
+      "thisserver":"${changeme}.shadowdock.com",
+      "docker": {
+          "privaterepo":"dockerpriv.shadowdock.com",
+          "username":"keithyau",
+          "password":"thomas123"
+      },
+      "deploycode":{
+         "basedirectory":"/home/keithyau/bootdev/shadowdock/"
+      }
+   },
+   "override_attributes":{
+
+   },
+   "chef_type":"role",
+   "run_list":[
+      "recipe[git]",
+      "recipe[build-essential]",
+      "recipe[basicservice]",
+      "recipe[glusterfs]",
+      "recipe[webserver]",
+      "recipe[deploycode]",
+      "recipe[bootdev_customdomain]"
+   ],
+   "env_run_lists":{
+
+   }
+}
+
+EOF
+
