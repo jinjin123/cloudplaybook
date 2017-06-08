8fe65d4c7fe1bc3ea0c4cebfff24471a0cccc6ab
keithyau <keithyau.hk@gmail.com>
2017-06-08 16:18:36 +0800

    Update bootkylin-init.sh

--- bootkylin-init.sh
+++ bootkylin-init.sh
@@ -4,6 +4,13 @@
 echo "SELINUX=disabled" > /etc/sysconfig/selinux
 setenforce 0
 
+#Enable release in EMR master AMI
+echo "releasever=latest" >> /etc/yum.conf
+
+#Fix yum cache
+rm -rf /var/cache
+mkdir /var/cache
+
 # Sync time
 CHECKING_NTPDATE=`command -v ntpdate|wc -l`
 if [ "$CHECKING_NTPDATE" != "0" ]; then
@@ -22,7 +29,7 @@ if [ "$CHECKING_CHEFSOLO" != "0" ]; then
     echo "Chef Solo exists"
 else
     echo "Installing Chef Solo"
-    /usr/bin/curl -L https://www.opscode.com/chef/install.sh | bash
+    /usr/bin/curl -L https://www.opscode.com/chef/install.sh | sh
 fi
 
 #Create Chef-repo
@@ -33,12 +40,13 @@ cd /root/kylin/chef/chef-repo
 
 #curl upgrade
 yum -y update
-CHECKING_GIT=`command -v git|wc -l`
-if [ "$CHECKING_GIT" != "0" ]; then
-    echo "GIT exists"
-else
-    yum -y install git
-fi
+yum -y install git
+#CHECKING_GIT=`command -v git|wc -l`
+#if [ "$CHECKING_GIT" != "0" ]; then
+#    echo "GIT exists"
+#else
+#    yum -y install git
+#fi
 
 #Must install
 yum -y install curl wget bind-utils
@@ -49,22 +57,17 @@ git clone -b docker-general-kyligence https://keithyau:thomas123@github.com/Kyli
 #prepare directories
 mkdir -p logs
 
-#Make this Linux Unique ID
-#thisuniqueid="$(dmidecode | grep -i uuid | head -1 | awk -F" " '{print $2}')"
-#Since dmidecode code cannot get unique id, temp use mac address
-#thisuniqueid=`ip addr show | grep ether | head -1 | awk -F" "  '{print $2}'`
-
-#get own IP and register to laravel, tell Laravel this server's password
-#ownip=`/usr/bin/curl --user "keithyau@163.com":thomas123 -F "linux_uid=${thisuniqueid}" -F 'spot_sshpass=thomas1234!' -F "spot_sshroot=root" http://d.bootdev.com/spot-register`
+#Key permission
+chmod 400 /root/kylin/chef/chef-repo/pem/clientemr.pem
 
 chef-solo -c ./settings/solo.rb -o "recipe[hadoop_files]" -j user_jsons/kap_emr_conf.json
 
-#start kap
-su hdfs
-cd /usr/local/kap/kap-2.3.7-GA-hbase1.x
-bin/kylin.sh start
+#start kap, need sparate action from UI
+#su hdfs
+#cd /usr/local/kap/kap-2.3.7-GA-hbase1.x
+#bin/kylin.sh start
 
 
-#OR 
+#OR
 #hadoop fs -mkdir /kylin
 #hadoop fs -chown root:hadoop /kylin
