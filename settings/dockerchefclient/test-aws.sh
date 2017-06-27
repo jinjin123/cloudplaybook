8c88f60108419a763bb0b4efcf747d8f71cfb2b4
root <root@kyclouddevchef.2up05uxto15uvecz2bhp3xi12g.bx.internal.chinacloudapp.cn>
2017-06-27 07:59:33 +0000

    Updating test-aws.sh script

--- settings/dockerchefclient/test-aws.sh
+++ settings/dockerchefclient/test-aws.sh
@@ -13,22 +13,11 @@
           },
           "kylin": {
             "kaptoken": "dda18812-e57b-47f1-8aae-38adebecde8a",
-            "identifier": "20170615000aws",
+            "identifier": "kycloud20170627aws",
             "region": "cn-north-1",
             "appType": "KAP+KyAnalyzer+Zeppelin",
             "keypair": "kylin",
-            "clusterLoginUserName": "admintest",
-            "clusterLoginPassword": "Kyligence2016",
-            "clusterName": "default",
-            "clusterType": "hbase",
-            "clusterVersion": "3.5",
-            "clusterWorkerNodeCount": 2,
-            "containerName": "default",
-            "edgeNodeSize": "Standard_D3_V2",
-            "metastoreName": "default",
-            "sshUserName": "admintest",
-            "sshPassword": "Kyligence2016",
-            "storageAccount": "20170615000awssa"
+            "edgeNodeSize": "Standard_D3_V2"
           }
         }
       }
