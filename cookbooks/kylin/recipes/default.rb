fa8c9723d96f57d533cabe3619b781db5ca602b2
Jinjin123 <1293813551@qq.com>
2017-07-12 14:36:39 +0800

    Ignore failure for user creation

--- cookbooks/kylin/recipes/default.rb
+++ cookbooks/kylin/recipes/default.rb
@@ -37,6 +37,7 @@ user 'hdfs' do
   gid 'root'
   home '/home/hdfs'
   shell '/bin/bash'
+  ignore_failure true
 end
 
 user 'hadoop' do
@@ -45,6 +46,7 @@ user 'hadoop' do
   gid 'root'
   home '/home/hadoop'
   shell '/bin/bash'
+  ignore_failure true
 end
 
 # Download and execute installation script
